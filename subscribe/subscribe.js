9aad9d3d35e86e603ae42015fdaf4af4d5089475
Stephen Serio <steph_stephen92@outlook.com>
2022-12-19 16:17:50 -0600

    FORMS ARE FINISHED!!

--- subscribe/subscribe.js
+++ subscribe/subscribe.js
@@ -1,17 +1,2 @@
-function handleSubmit(event) {
-    event.preventDefault();
-
-    const data = FormData(event.target);
-
-    const value = Object.fromEntries(data.entries());
-
-    console.log({ value });
-}
-
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
 
 
-const data = getFormDataAsJSON(form)
-
-doSomething(data.email, data.firstName, data.lastName);
