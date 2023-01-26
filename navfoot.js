1e55b079c74f5fc686fea725917ab04a25f4bc21
Stephen Serio <steph_stephen92@outlook.com>
2023-01-25 21:45:34 -0600

    added mobile comments for the navbar

---
+++ navfoot.js
@@ -0,0 +1,8 @@
+/* 
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const menu = document.getElementById("menu");
+
+hamburgerIcon.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+*/
