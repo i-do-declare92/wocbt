0dd2b92f9ce4ff71f3f882610634604b48ae4392
Ian Waddell <i.83.waddell@gmail.com>
2023-01-27 00:49:13 -0600

    Portrait mobile view hamburger navmenu done besides making it stickied to page top Also reorganized navfoot css

--- navfoot.js
+++ navfoot.js
@@ -1,8 +1,17 @@
-/* 
-const hamburgerIcon = document.getElementById("hamburger-icon");
-const menu = document.getElementById("menu");
 
-hamburgerIcon.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
-*/
+        function myFunction() {
+          document.getElementById("drop").classList.toggle("show");
+        }
+
+        window.onclick = function(event) {
+            if (!event.target.matches('.hamburger')) {
+                var dropdowns = document.getElementsByClassName("burger");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
